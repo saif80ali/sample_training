ef8e718c3a47b361ef9b5ffce886a676558c27f0
Saif Ali <76246690+saif80ali@users.noreply.github.com>
2022-04-05 11:13:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const connectToMogo = require("./db")
+const express = require('express')
+
+connectToMogo()
+const app = express()
+const port = 3000
+app.use(express.json())
+// app.get('/', (req, res) => {
+//   res.send('Hello World! Saif')
+// })
+app.use('/api/auth',require('./routes/auth'))
+app.use('/api/notes',require('./routes/notes'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
